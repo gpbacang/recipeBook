e5fdf74e62a2642630e924fa58e955f484ed5e99
Geraldine <geraldine@g-angle.co.jp>
2017-03-07 09:39:39 +0800

    Modify route to go to login page on start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './login/login.component';
 
 export const router: Routes = [
   { path: '', component: AuthenticationComponent, children: [
-    // { path: '', redirectTo: '/signup', pathMatch: 'full' }
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
   ]},
+  { path: 'login', component: LoginComponent }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
