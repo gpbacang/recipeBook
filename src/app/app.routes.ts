28c2722fa24fb72d6e3e0e17a767e45ab6a0c28b
Geraldine <geraldine@g-angle.co.jp>
2017-03-08 14:05:00 +0800

    Add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const router: Routes = [
   { path: '', component: AuthenticationComponent, children: [
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
   ]},
-  { path: 'login', component: LoginComponent }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
