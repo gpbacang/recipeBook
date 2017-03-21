d2098b702845d145d50d457f711288a1b614de7c
Geraldine <geraldine@g-angle.co.jp>
2017-03-21 11:38:22 +0800

    Modify search bar

--- src/app/protected/recipe-list/recipe-list.component.ts
+++ src/app/protected/recipe-list/recipe-list.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  inFocus: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  focusFunction() {
+    this.inFocus = true;
+  }
+
+  focusOutFunction() {
+    this.inFocus = false;
+  }
+
+  // setStyle() {
+  //   this.currentStyles = {
+  //     'border': this.inFocus ? '1px solid black' : '.5px solid rgba(0,0,0,0.3)',
+  //     'box-sizing': 'border-box'
+  //   };
+  // }
 
 }
